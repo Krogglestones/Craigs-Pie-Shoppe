44458ee2f9b9b3210a02e04e32a3f3a79081ecad
Craig Larson <crlarson68@gmail.com>
2020-03-07 15:09:40 -0800

    Add project files.

---
+++ CraigsPieShoppe/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraigsPieShoppe.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId=1, CategoryName="Fruit Pies", Description="Fresh fruit in a pie."},
+                new Category{CategoryId=2, CategoryName="Cheese Cakes", Description="Cheesy goodness."},
+                new Category{CategoryId=3, CategoryName="Seasonal Pies", Description="Pies of the season."}
+            };
+    }
+}
